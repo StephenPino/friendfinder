123a61c994b1898bfbf0c263cb757955d4d40e85
Stephen Pino <spino725@gmail.com>
2017-04-22 19:44:40 -0500

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+        "name": "Bobby",
+        "photo": "http://s3.amazonaws.com/hiphopdx-production/2016/04/Bobby-Shmurda-Video-e1461009423636.png",
+        "scores": [
+            1,
+            5,
+            1,
+            4,
+            5,
+            4,
+            4,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Phil",
+        "photo": "http://2045253e14zf1be2pd2k25gh.wpengine.netdna-cdn.com/wp-content/uploads/2014/03/Dr.-Phil-Becoming-One-of-mHealths-Biggest-Champions.jpeg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5
+        ]
+    }
+];
+
+module.exports = friends;
